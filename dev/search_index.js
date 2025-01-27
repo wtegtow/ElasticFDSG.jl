dd045981ba10be1daf02d2a4f193860c17c39ce5
Documenter.jl <documenter@juliadocs.github.io>
2025-01-27 20:11:47 +0000

    build based on 875c8cd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ElasticFDSG","category":"page"},{"location":"#ElasticFDSG","page":"Home","title":"ElasticFDSG","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ElasticFDSG.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ElasticFDSG]","category":"page"}]
+}
